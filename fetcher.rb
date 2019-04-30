b150b6c33f058b5f18fe95c9727569f2871b69b0
James Childers <childers@railroad19.com>
2019-04-30 11:16:36 -0500

    Add gemfile

--- fetcher.rb
+++ fetcher.rb
@@ -5,7 +5,7 @@ class Fetcher
 
   # fetch
   def fetch
-    HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
+    resp = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
   end
 end
 
